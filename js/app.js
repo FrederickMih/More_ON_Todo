5df204100c487af4af95f6d671c38bc9be0fbf5d
FrederickMih <fredtimermih@gmail.com>
2022-01-21 08:47:25 +0100

    Add addToDo function

--- js/app.js
+++ js/app.js
@@ -10,6 +10,21 @@ const UNCHECK = 'fa-circle-thin';
 const LINE_THROUGH = 'lineThrough';
 
 // Show today's date
-const options = {weekday:"long", month:"short", day:"numeric"};
+const options = { weekday: 'long', month: 'short', day: 'numeric' };
 const today = new Date();
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+dateElement.innerHTML = today.toLocaleDateString('en-US', options);
+
+// Add to do function
+
+function addToDo(toDo) {
+  const item = `
+                  <li class="item">
+                     <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                     <p class="text">${toDo}</p>
+                     <i class="fa fa-trash de"></i>   
+                  </li>
+                 `;
+  const position = 'beforeend';
+
+  list.insertAdjacentHTML(position, item);
+}
